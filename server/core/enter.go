d95deeaf34fb64d210a1b43be991b763a0705cbe
Gary <2388117544@qq.com>
2025-04-09 16:04:20 +0800

    修改只有开发环境下才注册代码生成插件

--- server/core/enter.go
+++ server/core/enter.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"gitee.com/mxcker/go-admin/server/core/global"
 	"gitee.com/mxcker/go-admin/server/core/internal/sys_admin"
 	"gitee.com/mxcker/go-admin/server/core/internal/sys_auth"
 	"gitee.com/mxcker/go-admin/server/core/internal/sys_autocode"
@@ -17,16 +18,19 @@ import (
 func Load(Server *gin.Engine) {
 	AdminAuthGroup := Server.Group("/api", middlewares.SysAuth)
 	PublicGroup := Server.Group("/api")
-	//注册系统组件
+	// 注册系统组件
 	moduleMap := new(models.ModuleMap)
-	moduleMap.Add("sys/autocode", sys_autocode.Register)
+	// 只有开发环境下注册自动生成代码
+	if global.IsDevelopment {
+		moduleMap.Add("sys/autocode", sys_autocode.Register)
+	}
 	moduleMap.Add("sys/global_variable", sys_global_variable.Register)
 	moduleMap.Add("sys/menu", sys_menu.Register)
 	moduleMap.Add("sys/role", sys_role.Register)
 	moduleMap.Add("sys/admin", sys_admin.Register)
 	moduleMap.Add("sys/auth", sys_auth.Register)
 	moduleMap.Add("sys/home", sys_home.Register)
-	//加载用户组件
+	// 加载用户组件
 	err := modules.Load(moduleMap)
 	if err != nil {
 		panic(err)
